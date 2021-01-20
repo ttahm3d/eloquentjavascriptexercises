1fffdf5f8ef896145828e28b635d5117f3adc5b6
ttahm3d <ttahm3d@gmail.com>
2021-01-20 13:28:14 +0530

    solution to chapter 2 exercises

---
+++ 02programstructure/01.js
@@ -0,0 +1,13 @@
+// Write a loop that makes seven calls to console.log to output the following triangle
+
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
+for (let s = "#"; s.length < 8; s += "#") {
+  console.log(s);
+}
